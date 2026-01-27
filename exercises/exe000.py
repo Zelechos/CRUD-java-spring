ed6c2bd5fe41ee1a28f066e5b89d079ee30d7809
Alex <zelechos@hotmail.com>
2026-01-26 23:57:04 -0300

    feat(v0.0.2): add code in exe000

--- exercises/exe000.py
+++ exercises/exe000.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 
+
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name())
+print(torch.cuda.current_device())
+
 # input (XOR inputs)
 X = torch.tensor([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
 print(X)
@@ -13,16 +18,56 @@ print(Y)
 class SimpleNN(nn.Module):
     def __init__(self):
         super(SimpleNN, self).__init__()
-        # Capa Lineal tambien conocida como capa densa o fully connected
-        # capa de entrada -> capa oculta
+
+        # Capa de entrada (2) → capa oculta (4)
         self.hidden = nn.Linear(2, 4)
-        # capa oculta -> capa de salida
+
+        # Capa oculta (4) → capa de salida (1)
         self.output = nn.Linear(4, 1)
-        # funcion de activacion Sigmoide
+
+        # Función de activación
         self.sigmoid = nn.Sigmoid()
 
-    # Funcion de entrenamiento del modelo
     def forward(self, x):
         x = self.sigmoid(self.hidden(x))
         x = self.sigmoid(self.output(x))
         return x
+
+
+model = SimpleNN()
+print(model)
+
+# Loss Function BCELoss
+criterion = nn.BCELoss()
+
+# Optimizer (Gradient descent) Adam
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+
+
+# Traning Loop
+epochs = 10000
+
+for epoch in range(epochs):
+
+    # Forward pass (Prediccion)
+    outputs = model(X)
+
+    # Calcular la perdida
+    loss = criterion(outputs, Y)
+
+    # Backpropagation
+    optimizer.zero_grad()  # para limpiar los gradientes
+    loss.backward()  # para calcular los gradientes
+    optimizer.step()  # para actualizar los pesos
+
+    if epoch % 50 == 0:
+        print(f"Epoch: [{epoch}/{epochs}], Loss: {loss.item():.6f}")
+
+
+# Probar el Model ya entrenado
+with torch.no_grad():
+    predictions = model(X)
+    print("\nPredicciones:")
+    print(predictions)
+    print("\nPredicciones redondeadas:")
+    print(torch.round(predictions))
